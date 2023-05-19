6f12b035798de691dcab9c16be0211e5c6d207a0
andres-alcala-gtz <hector.alcala2936@alumnos.udg.mx>
2023-05-19 12:23:15 -0600

    version 1.0.0

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector("#header-menu");
+let navbar = document.querySelector(".header-navbar");
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+
+    sections.forEach(section => {
+
+        let top = window.scrollY;
+        let height = section.offsetHeight;
+        let offset = section.offsetTop - document.querySelector("header").offsetHeight;
+        let id = section.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(navlink => {
+                navlink.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+            });
+        };
+
+    });
+
+    menu.classList.remove("bx-x");
+    navbar.classList.remove("active");
+
+};
